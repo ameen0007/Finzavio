6b523b82c96e7bcf9d0dd92444b8a7d60f5a2b89
ameen <ameencrews@gmail.com>
2025-01-09 16:41:14 +0530

    m

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -8,8 +8,8 @@ import { Hero } from './Hero/Hero'
 const Home = () => {
 
   return (
-  <div className='w-full bg-[#0b0b0b]'>
-      <div className='main-div w-full bg-[#0b0b0b] h-screen max-w-[1440px] m-auto '>
+  <div className='w-full min-h-screen pb-5 bg-[#0b0b0b]'>
+      <div className='main-div  bg-[#0b0b0b] w-full  h-auto  max-w-[1440px] m-auto '>
        <Header/>
          <Hero/>
     </div>
