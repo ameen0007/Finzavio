46c531da5e1879fafe9b35d826ebf70bd516d113
ameen <ameencrews@gmail.com>
2025-01-13 21:26:36 +0530

    check css

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -24,18 +24,18 @@ export const Hero = () => {
       <div className={`${bricolageGrotesque.className} flex gap-20 justify-between hero `}>
         {/* Left Section - Content */}
         <div className="w-full text flex flex-col justify-between">
-          <p className="text-5xl p text-[#f3f3f1] glow1 top-to-bottom delay-1 leading-tight tracking-normal">
-            Where <span className= " glow1 top-to-bottom delay-1 text-[#ffda46]">Peace and Happiness</span>
+          <p className="text-5xl p text-[#f3f3f1]  leading-tight tracking-normal">
+            Where <span className= "  text-[#ffda46]">Peace and Happiness</span>
           </p>
-          <p className="p text-5xl glow1 top-to-bottom delay-1 text-[#f3f3f1] leading-tight tracking-normal">
+          <p className="p text-5xl  text-[#f3f3f1] leading-tight tracking-normal">
             Meet Finance
           </p>
-          <p className={`${Ralewayf.className} mt-7 s glow1 top-to-bottom delay-4 text-white text-sm leading-7 tracking-wider`}>
+          <p className={`${Ralewayf.className} mt-7 s  text-white text-sm leading-7 tracking-wider`}>
             Discover a world where managing your finances brings clarity and joy. Our tailored
             solutions simplify your journey to financial stability, allowing you to focus on what
             matters most.
           </p>
-          <div className={`${Ralewayf.className} w-full h-9 glow1 top-to-bottom delay-6 flex justify-start buttons gap-7 mt-7 text-[#f3f3f1]`}>
+          <div className={`${Ralewayf.className} w-full h-9  flex justify-start buttons gap-7 mt-7 text-[#f3f3f1]`}>
             <div className="btn-1 active:scale-95">
               <p>Know More</p>
             </div>
@@ -60,16 +60,16 @@ export const Hero = () => {
           className="pl-10 glow1 images top-to-bottom object-contain delay-0 rounded-lg max-w-[520px]"
         />
         {/* Glowing Images with Animation */}
-        <div className="glow m1 top-to-bottom delay-2 absolute top-[-10%] right-[38%]">
+        <div className="glow m1 top-to-bottom delay-3 absolute top-[-10%] right-[38%]">
           <img src="gold.png" alt="gold" />
         </div>
-        <div className="glow m2 top-to-bottom delay-3 absolute bottom-[23%] left-[12%]">
+        <div className="glow m2 top-to-bottom delay-4 absolute bottom-[23%] left-[12%]">
           <img src="gold.png" alt="gold" />
         </div>
-        <div className="glow m3 top-to-bottom delay-2 absolute bottom-[20%] right-[16%]">
+        <div className="glow m3 top-to-bottom delay-5 absolute bottom-[20%] right-[16%]">
           <img src="gold.png" alt="gold" />
         </div>
-        <div className="glow m4 top-to-bottom delay-4 absolute bottom-[1%] right-[50%]">
+        <div className="glow m4 top-to-bottom delay-3 absolute bottom-[1%] right-[50%]">
           <img src="star.png" alt="star" />
         </div>
         <div className="glow m5 top-to-bottom delay-5 absolute top-[22%] left-[20%]">
@@ -81,7 +81,17 @@ export const Hero = () => {
         <div className="glow m7 top-to-bottom delay-7 absolute top-[14%] right-[15%]">
           <img src="bluel.png" alt="blue light" />
         </div>
+  
+          
+        
+   
+
+
+
+
       </div>
+
+      
     </div>
       <div className="pb-10 btn-3 text-[#f3f3f1]">
         <div className="items animate-fade-up-right delay-500">
